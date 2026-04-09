de4dc23440b16fde6f1fc69667e8919b395a7a43
Yashosw10 <yashosw10@gmail.com>
2026-04-09 16:03:46 +0530

    Update package.json and vite.config.js for deployment setup

--- Retail_APP/vite.config.js
+++ Retail_APP/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/retail_app/', // Must match your repo name
+  build: {
+    outDir: 'dist'
+  }
 })
